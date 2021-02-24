2f2539ee53b446e3fb9817447ccf77c54955dc7d
LucasBarbosaSilva <blucas.oficial@gmail.com>
2021-02-24 00:00:21 -0300

    migrating to Next.js and starting countdown

---
+++ moveit-next/src/pages/index.tsx
@@ -0,0 +1,30 @@
+import styles from '../styles/pages/Home.module.css'
+
+import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from '../components/Profile';
+import { CompletedChallengs } from '../components/CompletedChallengs';
+import { Countdown } from '../components/Countdown';
+
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Início | move.it</title> 
+      </Head>
+      <ExperienceBar/>
+
+      <section>
+        <div>
+          <Profile/>
+          <CompletedChallengs/>
+          <Countdown/>
+        </div>
+        <div>
+          
+        </div>
+      </section>
+    </div>
+  )
+}
